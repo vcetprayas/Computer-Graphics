89b5fba57effe5c99dd596e5fc9af02f17d32370
vcetprayas <139954260+vcetprayas@users.noreply.github.com>
2023-08-09 12:26:29 +0530

    Add files via upload

---
+++ Midpoint/MIDPOINT.C
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+void main()
+{
+int xc,yc,x,y,r,gd=DETECT,gm,d;
+x=0;
+initgraph(&gd,&gm,"C:\\turboc3\\bgi");
+printf("%d",&r);
+scanf("%d",&r);
+printf("Enter centre coordinates (x,y)");
+scanf("%d%d",&xc,&yc);
+d=1-r;
+y=r;
+do
+{
+putpixel(xc+x,yc+y,7);
+putpixel(xc+x,yc-y,7);
+putpixel(xc-x,yc+y,7);
+putpixel(xc-x,yc-y,7);
+putpixel(xc+y,yc+x,7);
+putpixel(xc+y,yc-x,7);
+putpixel(xc-y,yc+x,7);
+putpixel(xc-y,yc-x,7);
+if(d<0)
+{
+x++;
+d+=2*x+3;
+}
+else
+{
+x++;
+y--;
+d+=2*(x-y)+5;
+}
+}
+while(x<y);
+getch();
+closegraph();
+}
